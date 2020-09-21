e014ab8852b6ad45db4d35081e262548a9280675
yz <admin@javaweb.org>
2020-09-21 12:48:43 +0800

    修改测试的Servlet无法正确加载问题

--- javaweb-sec-source/javase/src/main/java/com/anbai/sec/servlet/TestServlet.java
+++ javaweb-sec-source/javase/src/main/java/com/anbai/sec/servlet/TestServlet.java
@@ -10,7 +10,8 @@ import java.io.PrintWriter;
  * Creator: yz
  * Date: 2019/12/14
  */
-// 如果使用注解方式请取消@WebServlet注释并注释掉web.xml中TestServlet相关配置
+// 如果使用注解方式请取消@WebServlet注释并注释掉web.xml中TestServlet相关配置，
+// 如果和SpringMVC一起配置注解的优先级会较低，导致无法访问，需要修改SpringMVC的Mapping。
 //@WebServlet(name = "TestServlet", urlPatterns = {"/TestServlet"})
 public class TestServlet extends HttpServlet {
 
