5c090a701971e0cdbb4714284df9fe9a1c4817bd
yz <admin@javaweb.org>
2020-09-27 00:27:59 +0800

    修改解析Class实现

--- javaweb-sec-source/javase/src/main/java/com/anbai/sec/bytecode/ClassByteCodeParser.java
+++ javaweb-sec-source/javase/src/main/java/com/anbai/sec/bytecode/ClassByteCodeParser.java
@@ -1,14 +1,21 @@
 package com.anbai.sec.bytecode;
 
-import java.io.*;
+import com.alibaba.fastjson.JSON;
+
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
 import static com.anbai.sec.bytecode.ClassByteCodeParser.Constant.*;
-import static org.javaweb.utils.HexUtils.bytes2HexString;
 
 public class ClassByteCodeParser {
 
+	/**
+	 * 枚举常量池类型，兼容到JDK15
+	 */
 	public enum Constant {
 		CONSTANT_UTF8(1, "A UTF-8 encoded Unicode string"),
 		CONSTANT_INTEGER(3, "An int literal value"),
@@ -46,30 +53,35 @@ public class ClassByteCodeParser {
 		}
 	}
 
-	public static byte[] readBytes(InputStream in, int len) throws IOException {
-		byte[] bytes = new byte[len];
-		in.read(bytes);
-		return bytes;
-	}
-
 	public static void main(String[] args) throws IOException {
-		File            classFile = new File("/Users/yz/IdeaProjects/javaweb-sec/javaweb-sec-source/javase/src/main/java/com/anbai/sec/classloader/TestHelloWorld.class");
-		DataInputStream dis       = new DataInputStream(new FileInputStream(classFile));
+		File classFile = new File("/Users/ly/IdeaProjects/javaweb-sec/javaweb-sec-source/javase/src/main/java/com/anbai/sec/classloader/TestHelloWorld.class");
+
+		// 转换为数据输入流
+		DataInputStream dis = new DataInputStream(new FileInputStream(classFile));
+
+		// 创建ByteCode解析结果Map
+		Map<String, Object> byteCodeMap = new LinkedHashMap<>();
 
 		// u4 magic;
-		String magic = bytes2HexString(readBytes(dis, 4));
+		int magic = dis.readInt();
 
 		// 校验文件魔数
-		if ("CAFEBABE".equals(magic)) {
+		if (0xCAFEBABE == magic) {
+			byteCodeMap.put("magic", magic);
+
 			// u2 minor_version
-			String minor = bytes2HexString(readBytes(dis, 2));
+			int minor = dis.readUnsignedShort();
+			byteCodeMap.put("minor", minor);
 
 			// u2 major_version;
-			String major = bytes2HexString(readBytes(dis, 2));
+			int major = dis.readUnsignedShort();
+			byteCodeMap.put("major", major);
 
 			// u2 constant_pool_count;
 			int poolCount = dis.readUnsignedShort();
+			byteCodeMap.put("poolCount", poolCount);
 
+			// 创建常量池Map
 			Map<Integer, Map<String, Object>> constantPoolMap = new LinkedHashMap<>();
 
 			// cp_info constant_pool[constant_pool_count-1];
@@ -289,34 +301,39 @@ public class ClassByteCodeParser {
 				constantPoolMap.put(i, map);
 			}
 
-//			for (Integer id : constantPoolMap.keySet()) {
-//				Map<String, Object> map      = constantPoolMap.get(id);
-//				Constant            constant = (Constant) map.get("tag");
-//
-//				if (constant == CONSTANT_UTF8) {
-//					System.out.println(new String((byte[]) map.get("bytes")));
-//				}
-//			}
+			byteCodeMap.put("constantPool", constantPoolMap);
 
 			// u2 access_flags;
 			int accessFlags = dis.readUnsignedShort();
+			byteCodeMap.put("accessFlags", accessFlags);
 
 			// u2 this_class;
 			int thisClass = dis.readUnsignedShort();
+			byteCodeMap.put("thisClass", thisClass);
 
 			// u2 super_class;
 			int superClass = dis.readUnsignedShort();
+			byteCodeMap.put("superClass", superClass);
 
 			// u2 interfaces_count;
 			int interfacesCount = dis.readUnsignedShort();
+			byteCodeMap.put("interfacesCount", interfacesCount);
+
+			// 创建接口Index数组
+			int[] interfaces = new int[interfacesCount];
 
 			// u2 interfaces[interfaces_count];
 			for (int i = 0; i < interfacesCount; i++) {
 				int index = dis.readUnsignedShort();
+
+				interfaces[i] = index;
 			}
+
+			byteCodeMap.put("interfaces", interfaces);
 
 			// u2 fields_count;
 			int fieldsCount = dis.readUnsignedShort();
+			byteCodeMap.put("fieldsCount", fieldsCount);
 
 			// field_info fields[fields_count];
 			for (int i = 0; i < fieldsCount; i++) {
@@ -328,19 +345,90 @@ public class ClassByteCodeParser {
 //					attribute_info attributes[attributes_count];
 //				}
 
-				int fieldAccessFlags     = dis.readUnsignedShort();
-				int fieldNameIndex       = dis.readUnsignedShort();
+				// u2 access_flags;
+				int fieldAccessFlags = dis.readUnsignedShort();
+
+				// u2 name_index;
+				int fieldNameIndex = dis.readUnsignedShort();
+
+				// u2 descriptor_index;
 				int fieldDescriptorIndex = dis.readUnsignedShort();
+
+				// u2 attributes_count;
 				int fieldAttributesCount = dis.readUnsignedShort();
 
-//				attribute_info {
-//					u2 attribute_name_index;
-//					u4 attribute_length;
-//					u1 info[attribute_length];
-//				}
+				readAttributes(fieldAttributesCount, dis);
 			}
+
+			// u2 methods_count;
+			int methodsCount = dis.readUnsignedShort();
+			byteCodeMap.put("methodsCount", methodsCount);
+
+			// method_info methods[methods_count];
+			for (int i = 0; i < methodsCount; i++) {
+//				method_info {
+//					u2 access_flags;
+//					u2 name_index;
+//					u2 descriptor_index;
+//					u2 attributes_count;
+//					attribute_info attributes[attributes_count];
+//				}
+
+				// u2 access_flags;
+				int methodAccessFlags = dis.readUnsignedShort();
+
+				// u2 name_index;
+				int methodNameIndex = dis.readUnsignedShort();
+
+				// u2 descriptor_index;
+				int methodDescriptorIndex = dis.readUnsignedShort();
+
+				// u2 attributes_count;
+				int methodAttributesCount = dis.readUnsignedShort();
+
+				// attribute_info attributes[attributes_count];
+				readAttributes(methodAttributesCount, dis);
+			}
+
+			// u2 attributes_count;
+			int attributesCount = dis.readUnsignedShort();
+			byteCodeMap.put("attributesCount", attributesCount);
+
+			// attribute_info attributes[attributes_count];
+			readAttributes(attributesCount, dis);
+
+			System.out.println(JSON.toJSONString(byteCodeMap));
 		} else {
 			throw new RuntimeException("Class文件格式错误!");
+		}
+	}
+
+	/**
+	 * 解析Attributes
+	 *
+	 * @param attributesCount Attributes数量
+	 * @param dis             DataInputStream对象
+	 * @throws IOException 读取数据IO异常
+	 */
+	private static void readAttributes(int attributesCount, DataInputStream dis) throws IOException {
+		// attribute_info attributes[attributes_count];
+		for (int j = 0; j < attributesCount; j++) {
+//					attribute_info {
+//						u2 attribute_name_index;
+//						u4 attribute_length;
+//						u1 info[attribute_length];
+//					}
+
+			// u2 attribute_name_index;
+			int attributeNameIndex = dis.readUnsignedShort();
+
+			// u4 attribute_length;
+			int attributeLength = dis.readInt();
+
+			byte[] bytes = new byte[attributeLength];
+
+			// u1 info[attribute_length];
+			dis.read(bytes);
 		}
 	}
 
